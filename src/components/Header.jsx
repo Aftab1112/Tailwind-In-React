9c79f815ba9e3e9b40a5d2a0278f3ded220313eb
Aftab1112 <you@example.com>
2024-02-28 14:35:25 +0530

    added a simple header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import viteLogo from "/vite.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={viteLogo} alt="viteLogo" />
+    </header>
+  );
+}
