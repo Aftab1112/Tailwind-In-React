5432f04f1f525fcef3b4f8f8a3709bbffca1dbbf
Aftab1112 <you@example.com>
2024-03-03 08:33:21 +0530

    created a simple header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import viteLogo from "/vite.svg";
+import reactlogo from "/src/assets/react.svg";
 
 export default function Header() {
   return (
-    <header>
-      <img src={viteLogo} alt="viteLogo" />
+    <header className="flex justify-between py-4 px-4 md:px-8 shadow-md ">
+      <img src={reactlogo} alt="logo" />
+      <ul className="flex gap-4">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
     </header>
   );
 }
